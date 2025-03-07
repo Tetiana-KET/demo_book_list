dfb7c170625c1dcab7829c0e300525a17aac96d8
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2025-03-07 17:57:38 +0200

    chore: set configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+		modules: {
+			localsConvention: 'camelCase',
+		},
+	},
+	build: {
+		minify: 'esbuild',
+	},
+	resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@consts': path.resolve(__dirname, 'src/consts'),
+			'@services': path.resolve(__dirname, 'src/services'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@pages': path.resolve(__dirname, 'src/pages')
+    }
+  }
 })
